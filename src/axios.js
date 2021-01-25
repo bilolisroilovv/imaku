464ee001ec6b8978b56afd3ea06f8d6de99bfdeb
bilolisroilovv <bilolisroilovv@gmail.com>
2021-01-25 13:03:49 +0500

    update host

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://192.168.5.71:8000/'
+axios.defaults.baseURL = 'http://185.230.204.17/'
 /* axios.defaults.headers.common['Authorization'] = localStorage.getItem('token') */
 axios.defaults.headers.common = {
   Authorization: localStorage.getItem('token')
