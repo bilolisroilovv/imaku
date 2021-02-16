252a87dc0525bc526ade0bb5d427ebccc820663b
bilolisroilovv <bilolisroilovv@gmail.com>
2021-02-16 15:01:11 +0500

    da

--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-axios.defaults.baseURL = "http://192.168.5.56:8000/";
-/* axios.defaults.baseURL = "https://imaku.uz:8000/"; */
-=======
-// axios.defaults.baseURL = "http://192.168.5.56:8000/";
+/* axios.defaults.baseURL = "http://192.168.5.56:8000/"; */
 axios.defaults.baseURL = "https://imaku.uz:8000/";
->>>>>>> d15b93892aef5456c3052deef7528af199cfc252
 
 axios.defaults.headers.common = {
   Authorization: localStorage.getItem("token")
