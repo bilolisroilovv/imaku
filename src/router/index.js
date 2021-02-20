ea73c473d1e8d4ffdcc43690f7dcac96618b50bf
bilolisroilovv <bilolisroilovv@gmail.com>
2021-02-20 17:47:52 +0500

    update

--- src/router/index.js
+++ src/router/index.js
@@ -99,7 +99,7 @@ const routes = [
         component: () => import("../views/lite/desktop/CategoryPage.vue")
       },
       {
-        path: "search/:query/Ulugbek-Tursunkulov-Pizdobol",
+        path: "search/:query",
         name: "SearchPage",
         props: true,
         meta: { layout: "second" },
