4450ba04edfa4b9c332461a977916c31f048c71c
bilolisroilovv <bilolisroilovv@gmail.com>
2021-02-22 12:40:51 +0500

    update lang

--- src/components/lite/desktop/SearchGroup.vue
+++ src/components/lite/desktop/SearchGroup.vue
@@ -96,7 +96,10 @@ export default {
       inputPlaceholder: "Поиск объявлений, магазинов, аккаунтов",
       runtimeTranscription: "",
       transcription: [],
-      lang: ["uz-UZ", "ru-RU"],
+      lang: {
+        "uz-UZ",
+        "ru-RU"
+      },
       searchContent: null,
       error: false,
       speaking: false,
