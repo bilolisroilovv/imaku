75da0eac8988db5548404c6b66d42e9c4c080b0d
bilolisroilovv <bilolisroilovv@gmail.com>
2021-03-05 19:13:04 +0500

    update

--- vue.config.js
+++ vue.config.js
@@ -6,16 +6,5 @@ module.exports = {
       localeDir: "locales",
       enableInSFC: false
     }
-  },
-  chainWebpack: config => {
-    config.plugin('preload').tap(options => {
-      options[0].as = (entry) => {
-        if (/\.css$/.test(entry)) return 'style';
-        return 'script';
-      }
-      // options[0].fileWhitelist: [/\.files/, /\.to/, /\.include/]
-      // options[0].fileBlacklist: [/\.files/, /\.to/, /\.exclude/]
-      return options
-    })
   }
 };
