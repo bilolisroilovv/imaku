b87aabf93ab0dba61787d3394c3e40a404cfe697
Sanjar Rajabov <sanjar.rajabov06@gmail.com>
2021-04-02 19:37:42 +0500

    image changed

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* axios.defaults.baseURL = "https://imaku.uz/api"; */
 
-axios.defaults.baseURL = "https://imaku.uz/api/";
+axios.defaults.baseURL = "http://172.16.20.20/api/";
 
 axios.defaults.headers.common = {
   Authorization: localStorage.getItem("token")
