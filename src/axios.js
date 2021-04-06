24fd4b042b756fab575b7734e922309fbb4e89f4
bilolisroilovv <bilolisroilovv@gmail.com>
2021-04-06 11:16:41 +0500

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-/* axios.defaults.baseURL = "https://imaku.uz/api"; */
-axios.defaults.baseURL = "/api/";
+axios.defaults.baseURL = "https://imaku.uz/api";
+/* axios.defaults.baseURL = "/api/"; */
 
 axios.defaults.headers.common = {
   Authorization: localStorage.getItem("token")
